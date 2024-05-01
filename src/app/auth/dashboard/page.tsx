93bb334f4f27407fba570b246d98b56382ef5925
zobkazi <zobaidulkazi64.official@gmail.com>
2024-05-01 22:36:54 +0600

    DashboardLeftMenu added

--- src/app/auth/dashboard/page.tsx
+++ src/app/auth/dashboard/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Button } from 'antd'
+import DashboardLeftMenu from '@/components/dashboard/DashboardLeftMenu'
 
 
 function DashboardPage() {
   return (
       <div>
-          <Button className='' type="primary">Primary Button</Button>
-          
-          <Button type="link">Link Button</Button>
+
+      <DashboardLeftMenu onItemClick={undefined} />
     </div>
   )
 }
