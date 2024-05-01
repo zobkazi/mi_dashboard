2b38dde56d4920567a38ea81c33ffb7443f4f776
zobkazi <zobaidulkazi64.official@gmail.com>
2024-05-01 22:04:41 +0600

    DashboardPage added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'antd'
+
+
+function DashboardPage() {
+  return (
+      <div>
+          <Button className='' type="primary">Primary Button</Button>
+          
+          <Button type="link">Link Button</Button>
+    </div>
+  )
+}
+
+export default DashboardPage
